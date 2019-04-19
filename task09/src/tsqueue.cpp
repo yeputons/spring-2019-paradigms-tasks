de350b367897380569c428d70674dc76ba00ec68
Egor Suvorov <egor.suvorov@gmail.com>
2019-04-19 20:40:26 +0300

    task09: add initial template

---
+++ task09/src/tsqueue.cpp
@@ -0,0 +1,17 @@
+#include "tsqueue.h"
+
+void threadsafe_queue_init(ThreadsafeQueue *q) {
+    // TODO
+}
+
+void threadsafe_queue_destroy(ThreadsafeQueue *q) {
+    // TODO
+}
+
+void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
+    // TODO
+}
+
+void* threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
+    // TODO
+}
